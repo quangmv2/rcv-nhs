b517e7fbc1aec7484abf73cf6cf6db367933d4c1
Nga Quynh <quynhnganav@gmail.com>
2021-03-10 17:18:28 +0700

    .

--- src/pages/admin/questions/index.tsx
+++ src/pages/admin/questions/index.tsx
@@ -1,11 +1,39 @@
 import React, { memo } from "react";
+import { Table, Row, Col,} from 'antd';
+import QuestionForm from "./questionForm";
 
-const Home = () => {
+const Questions = () => {
+    const columns = [
+        {
+          title: 'Question',
+          dataIndex: 'question',
+          key: 'question',
+        },
+        {
+          title: 'Answers',
+          dataIndex: 'answers',
+          key: 'answer',
+        },
+        {
+          title: 'Answer',
+          dataIndex: 'answer',
+          key: 'answer',
+        },
+        {
+            title: 'Current Time',
+            dataIndex: 'current time',
+            key: 'current',
+          },
+        
+      ];
     return(
-        <div>
-            Question Page
-        </div>
+        <Row >
+             <Col xs={{ span: 18 }} lg={{ span: 16}} md={{ span: 24 }}> <Table columns={columns} /></Col>
+             <Col xs={{ span: 6 }} lg={{ span: 8}} md={{ span: 24 }}>  <QuestionForm /></Col>
+            
+       
+        </Row>
     )
 }
 
-export default memo(Home);
+export default memo(Questions);
