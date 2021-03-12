21efb0d92def5c84072a8119fe4c0522c1731327
quynhnganav <quynhnganav@gmail.com>
2021-03-12 11:46:23 +0700

    .

--- src/pages/contest/counter.tsx
+++ src/pages/contest/counter.tsx
@@ -1,19 +1,19 @@
 import React, { memo } from "react";
 
-import {Animated} from "react-animated-css";
+import { Animated } from "react-animated-css";
 import './contest.scss';
 
 const Counter = () => {
     return (
         <div className='timer-container'>
-        <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
-        
-           <span className='number'>10</span>
-           
-      
-    </Animated>
-    <p>SECONDS</p>
-    </div>
+            <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+
+                <span className='number'>10</span>
+
+
+            </Animated>
+            <p>SECONDS</p>
+        </div>
 
 
 
--- src/pages/contest/index.tsx
+++ src/pages/contest/index.tsx
@@ -4,15 +4,22 @@ import { Table, Row, Col, } from 'antd';
 import './contest.scss';
 import Answer from "./answer";
 import Counter from "./counter";
+import { RightCircleOutlined } from '@ant-design/icons';
 
 const Home = () => {
     return (
         <Row>
             <Col span={20}> <div className='container'>
+                <div className='counter-container'>
+                <Counter />
+            </div>
                 <Question question='hello' />
                 <Answer />
             </div>  </Col>
-            <Col span={4} className='counter'><Counter /></Col>
+            <Col span={4} className='counter'>
+            <RightCircleOutlined />
+                <span>Nga</span>
+                </Col>
 
         </Row>
 
